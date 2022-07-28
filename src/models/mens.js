423e747ee591a7e56dee1d0092c985c6c85da8c4
Rafayasad <asadrafay998@gmail.com>
2022-07-28 11:26:13 +0500

    pass bycrypt

--- src/models/mens.js
+++ src/models/mens.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const mens = mongoose.Schema({
-    ranking:{
-        type:String
+    ranking: {
+        type: String
     },
-    name:{
-        type:String
+    name: {
+        type: String
+    },
+    password: {
+        type: String
     }
 })
+
+//for secure password by hashing bcrypt
+
+mens.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        console.log(`this is my current password=>${this.password}`);
+        this.password = await bcrypt.hash(this.password, 10);
+        console.log(`this is my hash current password=>${this.password}`);
+    }
+    next();
+})
+
+
 
 const mensRanking = mongoose.model('ManRanking', mens);
 
